531d0218dbcc35b61e3ee1dbab480e556a0c0e9b
Amine Rguig <rguigmohamedamine@gmail.com>
2024-03-05 20:29:36 +0100

    First Commit

---
+++ constants/data.js
@@ -0,0 +1,118 @@
+const userData = {
+  githubUsername: "amirgu",
+  name: "Amine Rguig",
+  designation: "Data Scientist",
+  avatarUrl: "/avatar.png",
+  email: "aminerguig123@gmail.com",
+  phone: "+33 7 56 89 93 85",
+  address: "Paris, France.",
+  projects: [
+    {
+      title: "Football Betting Project",
+      link: "https://football-page-coral.vercel.app/",
+      imgUrl: "/football-page.png",
+    },
+    {
+      title: "Tableau",
+      link: "https://public.tableau.com/app/profile/amine.rguig/vizzes",
+      imgUrl: "/tableau-project.png",
+    },
+    {
+      title: "Computer Vision",
+      link: "https://github.com/Amirgu/MASH-Object-Recognition",
+      imgUrl: "/Object-Recognition.png",
+    },
+    {
+    title: "Deconvolution",
+     link: "https://github.com/Amirgu/SAS-DECONVOLUTION",
+       imgUrl: "/Deconvolution.png",
+     },
+    {
+      title: "NLP",
+       link: "https://github.com/Amirgu/NLP-proj",
+       imgUrl: "/algochurn.png",
+     },
+    // {
+    //   title: "Playground",
+    //   link: "https://play.placeholdertech.in",
+    //   imgUrl: "/playground.png",
+    // },
+    // {
+    //   title: "Feedmeback",
+    //   link: "https://feedmeback.vercel.app",
+    //   imgUrl: "/feedmeback.png",
+    // },
+    // {
+    //   title: "Devmedium",
+    //   link: "https://devmedium.vercel.app",
+    //   imgUrl: "/devmedium.png",
+    // },
+  ],
+  about: {
+    title:
+      "I'm a Data Scientist/Analyst that loves to see through the data to unveal patterns and insights",
+    description: [
+      `I've been captivated by mathematics since childhood, which naturally propelled me towards a career centered around math and statistics. Additionally, my passion for football is evident through the projects showcased on this site. I thoroughly enjoy combining the practical with the pleasurable, indulging in football analytics as a personal passion.`,
+      `I love to learn and take on new challenges, much like doing this site, where I constantly strive to expand my knowledge and skills. `,
+      `I've been coding in Python, R, SAS, MySQL, PostgreSQL, and C++. Despite the challenges, I approach coding with confidence and consider myself a skilled developer, continuously honing my craft with a sense of pride and determination.`,
+    ],
+    // currentProject: "Tailwind Master Kit",
+    // currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Data Scientist",
+      company: "Société Générale",
+      year: "July 2023 - December 2023",
+      // companyLink: "https://mroads.com",
+      desc: "I worked as a Data Scientist focusing on enhancing fraud detection and money laundering prevention. Using advanced graph neural networks like GraphSAGE, GCN, and GAT, I developed models and visualization tools to analyze diverse relationships between entities such as companies and individuals, helping to uncover fraudulent patterns effectively.",
+    },
+    {
+      title: "Student",
+      company: "Université Paris-Dauphine",
+      year: "2022 - 2023",
+      
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Data Scientist",
+      company: "OCP Group",
+      year: "June 2022 - September 2022",
+      // companyLink: "https://algochurn.com",
+      desc: "I focused on improving data quality and precision through thorough cleaning and preprocessing tasks. Utilizing Python, Pandas, Scikit-learn, and SQL, I implemented an optimized algorithm to identify and execute efficiency enhancements, resulting in reduced machine travel distances and lower CO2 emissions.",
+    },
+    {
+      title: "Student",
+      company: "Sorbonne Université",
+      year: "2021 - 2022",
+      // companyLink: "https://intel.com",
+      desc: "Master 1 in Applied Mathematics : Advanced Statistics, Probabilities and basic Machine Learning Concepts",
+    },
+    {
+      title: "Student",
+      company: "Université de Lorraine",
+      year: "2020 - 2021",
+      // companyLink: "https://upes.co.in",
+      desc: "Bachelor Degree in Applied Mathematics",
+    },
+    {
+      title: "Student",
+      company: "Enseirb-Matmeca",
+      year: "2019 - 2020",
+      // companyLink: "https://upes.co.in",
+      desc: "1st year cycle of engineer in computer science",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    // instagram: "https://instagram.com/maninthere",
+    // twitter: "https://twitter.com/mannupaaji",
+    linkedin: "https://www.linkedin.com/in/amine-rguig-567427265/",
+    github: "https://github.com/amirgu",
+    // facebook: "https://facebook.com/",
+    // peerlist: "https://peerlist.io/manuarora",
+  },
+};
+
+export default userData;
