e084bc8979ca013bf476306d45e1acf9de097c6f
Amine Rguig <rguigmohamedamine@gmail.com>
2024-03-05 22:13:33 +0100

    Add Visitor Tracking

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
 import getLatestRepos from "@lib/getLatestRepos";
 import userData from "@constants/data";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home({ repositories }) {
   return (
@@ -16,7 +17,9 @@ export default function Home({ repositories }) {
       <Hero />
       <FavouriteProjects />
       <LatestCode repositories={repositories} />
+      <Analytics />
     </ContainerBlock>
+    
   );
 }
 
